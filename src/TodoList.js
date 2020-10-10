f7bc36553e5f57cba9994fcaeb29485f5fcfa465
laur80 <laur80raul@gmail.com>
2020-10-10 23:12:10 +0300

    splitting into 2 contexts

--- src/TodoList.js
+++ src/TodoList.js
@@ -7,7 +7,7 @@ import Divider from '@material-ui/core/Divider';
 
 
 function TodoList() {
- const {todos}= useContext(TodosContext);
+ const todos= useContext(TodosContext);
  if(todos.length)
   return (
     <Paper>
--- src/context/todos.context.js
+++ src/context/todos.context.js
@@ -1,19 +1,22 @@
 import todoReducer from "../reducers/todo.reducer"
 import React,{createContext, useReducer} from 'react'
-import useTodoState from "../hooks/useTodoState"
+
 
 const defaultTodos=[
  {id:1, task:"learn", completed: true },
  {id:2, task:"work", completed: false }
 ];
 export const TodosContext=createContext();
+export const DispatchContext=createContext();
 
 export function TodosProvider(props) {
  const [todos,dispatch] = useReducer(todoReducer,defaultTodos);
  
   return (
-   <TodosContext.Provider value={{todos,dispatch}}>
+   <TodosContext.Provider value={todos}>
+   <DispatchContext.Provider value={dispatch}>
     {props.children}
+   </DispatchContext.Provider>
    </TodosContext.Provider>
   )
  }
