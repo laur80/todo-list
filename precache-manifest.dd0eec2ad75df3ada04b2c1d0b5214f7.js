99b942b736595ebcab5f50911ab21537a09824cd
laur80 <laur80raul@gmail.com>
2020-10-11 11:06:30 +0300

    Updates

---
+++ precache-manifest.dd0eec2ad75df3ada04b2c1d0b5214f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913655eb7d3bd5f6b2852a99f8a0e85c",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "0f56bccc1f08157dd263",
+    "url": "/todo-list/static/js/2.0d9a349a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-list/static/js/2.0d9a349a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e684df37ad06516ac278",
+    "url": "/todo-list/static/js/main.91dee84b.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
